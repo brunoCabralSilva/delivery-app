6a6b1f8cadf5d1b82d49f21317f474c9a0ac33eb
marlloncps <marllon.cps@gmail.com>
2022-12-01 18:20:30 -0300

    back-end dos requisitos 9 e 10

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const login = require('../routes/login');
+const register = require('../routes/register');
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/login', login);
+app.use('/register', register);
 
 module.exports = app;
