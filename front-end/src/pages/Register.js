3634631a4c5089f786d37d1fc3668055448bf828
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-12-02 21:20:27 -0300

    feat: register page

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,3 +1,31 @@
+import { useState } from 'react';
+
 export default function Register() {
-  return (<p>Register</p>);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div>
+      <input
+        type="name"
+        value={ name }
+        onChange={ (e) => setName(e.target.value) }
+        data-testid="common_register__input-name"
+      />
+      <input
+        type="email"
+        value={ email }
+        onChange={ (e) => setEmail(e.target.value) }
+        data-testid="common_register__input-email"
+      />
+      <input
+        type="password"
+        value={ password }
+        onChange={ (e) => setPassword(e.target.value) }
+        data-testid="common_register__input-password"
+      />
+      <div data-testid="common_register__element-invalid_register" />
+    </div>
+  );
 }
