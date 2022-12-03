d269a7cbf260e559a028f45ec82080b3b7cb0869
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-12-03 19:33:34 -0300

    requisito 8

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,9 +1,34 @@
 import { useState } from 'react';
+// import { useHistory } from 'react-router-dom';
 
 export default function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [validation, setValidation] = useState(false);
+  const CREATED_STATUS = 201;
+  // const history = useHistory();
+
+  const enableButton = () => {
+    const number = 6;
+    const validateEmail = /\S+@\S+\.\S+/;
+    const vEmail = !email || !validateEmail.test(email) || email === '';
+    const vPassword = !password || password.length < number;
+    const vName = !name || name === '' || name.length < 2 * number;
+    return vEmail || vPassword || vName;
+  };
+
+  const validadeUser = async () => {
+    try {
+      const vUser = await axios.post('http://localhost:3001/register', { name, email, password });
+      if (vUser.status === CREATED_STATUS) {
+        setValidation(false);
+      }
+    } catch (error) {
+      console.log(error.message);
+      setValidation(true);
+    }
+  };
 
   return (
     <div>
@@ -26,13 +51,21 @@ export default function Register() {
         data-testid="common_register__input-password"
       />
       <button
-        onClick={ () => console.log('Deu') }
+        disabled={ enableButton() }
+        onClick={ validadeUser }
         type="button"
         data-testid="common_register__button-register"
       >
-        Login
+        Cadastrar
       </button>
-      <div data-testid="common_register__element-invalid_register" />
+      {
+        validation
+          ? (
+            <p data-testid="common_register__element-invalid_register">
+              Usuário Inválido
+            </p>)
+          : null
+      }
     </div>
   );
 }
