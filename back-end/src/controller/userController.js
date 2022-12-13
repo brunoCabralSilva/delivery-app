8137adbf6b92e891ebba2ef9552e45b2d9e6fab6
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-12-13 15:02:10 -0300

    [Grupo 9] Rota Manager Backend

--- back-end/src/controller/userController.js
+++ back-end/src/controller/userController.js
@@ -1,4 +1,5 @@
-const { getUser } = require('../service/userService');
+const { getUser, findAll, create, deleteUser } = require('../service/userService');
+const JwtAuth = require('../utils/Authentication');
 
 const returnIdUser = async (req, res) => {
   const { email } = req.params;
@@ -7,4 +8,28 @@ const returnIdUser = async (req, res) => {
   return res.status(404).json({ message: 'Usuário não existe no banco de dados' });
 };
 
-module.exports = { returnIdUser };
+const returnAllUsers = async (req, res) => {
+  const users = await findAll();
+  if (users) return res.status(200).json(users);
+  return res.status(404).json({ message: 'Banco de dados sem usuários' });
+};
+
+const createUser = async (req, res) => {
+  if (JwtAuth.validation(req.headers.authorization)) {
+  const user = await create();
+  if (user) return res.status(200).send('Usuário criado com sucesso');
+  }
+  return res.status(400).send('Token not found');
+};
+
+const deleteUsers = async (req, res) => {
+  const { id } = req.params;
+  if (JwtAuth.validation(req.headers.authorization)) {
+  const user = await deleteUser(id);
+  if (user) return res.status(200).send('Usuário deletado com sucesso');
+  return res.status(404).send('Falha ao excluir usuário');
+  }
+  return res.status(400).send('Token not found');
+};
+
+module.exports = { returnIdUser, returnAllUsers, createUser, deleteUsers };
